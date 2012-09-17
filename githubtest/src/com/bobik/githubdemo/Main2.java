dae4b387a54c41375254bb62add07bcff57499be
mankoSergiy <manko.sergiy@gmail.com>
2012-09-17 23:56:27 +0300

    sdasdasdasd

--- githubtest/src/com/bobik/githubdemo/Main2.java
+++ githubtest/src/com/bobik/githubdemo/Main2.java
@@ -7,7 +7,7 @@ public class Main2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello World hallo!");
 	}
 
 }
